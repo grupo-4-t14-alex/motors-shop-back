da2cc4c3bb5e32a5514ce06cce5b468c7299a701
Sarah Venske Costa <sarahvenske@gmail.com>
2023-06-06 15:40:00 -0300

    feat: folder structure created

--- src/controllers/users.controllers.ts
+++
@@ -1,8 +0,0 @@
-import { Response, Request } from "express";
-
-const createUsersController =  async( request : Request, response : Response ):Promise<Response> => {
-
-    return await response.status(201).json({message: "deu certo"})
-}
-
-export {createUsersController}
