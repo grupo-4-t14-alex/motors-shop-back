270bf50b8ea95f1e8ef8d56d50a6c374b82c8a39
Sarah Venske Costa <sarahvenske@gmail.com>
2023-06-06 10:17:14 -0300

    feat: cars entity created

---
+++ src/entities/cars.entity.ts
@@ -0,0 +1,47 @@
+import { 
+    Column, 
+    Entity, 
+    PrimaryGeneratedColumn 
+} from "typeorm";
+
+@Entity("cars")
+class Car {
+
+    @PrimaryGeneratedColumn("increment")
+    id: number
+
+    @Column({ length: 50 })
+    brand: string
+
+    @Column({ length: 120 })
+    model: string
+
+    @Column({ type: "int" })
+    year: number
+
+    @Column({ length: 50 })
+    fuel: string
+
+    @Column({ length: 7 })
+    km: string
+
+    @Column({ length: 50 })
+    color: string
+
+    @Column({ type: "decimal", precision: 12, scale: 2, default: 0 })
+    fipePrice: number | string
+
+    @Column({ type: "decimal", precision: 12, scale: 2, default: 0 })
+    sellPrice: number
+
+    @Column({ type: "text" })
+    description: string
+
+    @Column({ type: "boolean", default: true })
+    isActive: boolean
+
+}
+
+export {
+    Car
+}
---
+++ src/entities/index.ts
@@ -0,0 +1,5 @@
+import { Car } from "./cars.entity";
+
+export {
+    Car
+}
