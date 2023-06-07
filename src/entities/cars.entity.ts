fd4313538582b53c7f56d7c1fa26d5203899cad6
Lucas Araujo <lucas984618740@gmail.com>
2023-06-07 13:16:50 -0300

    feat:update Service

--- src/entities/cars.entity.ts
+++ src/entities/cars.entity.ts
@@ -5,41 +5,40 @@ import {
     ManyToOne, 
     PrimaryGeneratedColumn 
 } from "typeorm";
-import { User } from "./users.entity";
 
 @Entity("cars")
 class Car {
-
+    
     @PrimaryGeneratedColumn("increment")
     id: number
-
+    
     @Column({ length: 50 })
     brand: string
-
+    
     @Column({ length: 120 })
     model: string
-
+    
     @Column({ type: "int" })
     year: number
-
+    
     @Column({ type: "int" })
     fuel: number
-
+    
     @Column({ length: 7 })
     km: string
-
+    
     @Column({ length: 50 })
     color: string
-
+    
     @Column({ type: "int"})
     fipePrice: number
-
+    
     @Column({ type: "int"})
     sellPrice: number
-
+    
     @Column({ type: "text" })
     description: string
-
+    
     @Column({ type: "boolean", default: true })
     isActive: boolean
     
@@ -47,9 +46,6 @@ class Car {
     updateStatus() {
         this.isActive = false
     }
-
-    @ManyToOne(() => User, (user)=> user.cars)
-    owner: User
 }
 
 export {
--- src/routers/cars.routes.ts
+++ src/routers/cars.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createCarController, deleteCarsControllers } from "../controllers";
+import { updateCarController } from "../controllers/cars/updateCar.controllers";
 
 
 const carRoutes : Router = Router()
 
 carRoutes.post("", createCarController)
+carRoutes.patch("/:CarId", updateCarController)
 carRoutes.delete("", deleteCarsControllers)
 
 export default carRoutes
