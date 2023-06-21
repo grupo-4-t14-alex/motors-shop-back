9dab3c9b0979391e44cc3972adf8058a527f71f6
unknown <jonatasilveira04@gmail.com>
2023-06-21 14:54:01 -0300

    feat: changed return from retrieve cars route

--- src/services/cars/retriveCars.service.ts
+++ src/services/cars/retriveCars.service.ts
@@ -6,19 +6,33 @@ import { ICar, IResultCars } from "../../interfaces/cars.interfaces";
 
 export const retriveCarsService = async (userId: number): Promise<ICar[]> => {
   const userRepository: Repository<User> = AppDataSource.getRepository(User);
+  const carsRepository: Repository<Car> = AppDataSource.getRepository(Car)
 
-  const user: User | null = await userRepository.findOne({
-    where: {
-      id: userId,
-    },
-    relations: {
-      cars: true,
-    },
-  });
+  const user: User | null = await userRepository.findOneBy({id: userId});
 
   if (!user) {
     throw new AppError("User not exists", 404);
   }
 
-  return user.cars;
+  /* const cars = await userRepository.find({
+    where: {
+      id: userId
+    },
+    relations: {
+      cars: true
+    }
+  }) */
+
+  const cars: Car[] = await carsRepository.createQueryBuilder("cars")
+  .leftJoinAndSelect("cars.user", "user", `user.id = ${userId}`)
+  .addSelect("user.password", "password")
+  .addSelect("user.cpf", "cpf")
+  .addSelect("user.phone", "phone")
+  .addSelect("user.birthDate", "birthDate")
+  .addSelect("user.admin", "admin")
+  .addSelect("user.email", "email")
+  .getMany()
+
+
+  return cars;
 };
