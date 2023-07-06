6258aa0c3a5a85a44d95d4e75db172f5500d0afd
Sarah Venske Costa <sarahvenske@gmail.com>
2023-07-06 15:30:41 -0300

    fix: fixed swagger documentation

--- src/routers/swagger.routes.ts
+++ src/routers/swagger.routes.ts
@@ -1,35 +1,42 @@
 import { Router } from "express";
+import { userRoutes } from "./users.routes";
 import { 
-    createCarController,
-    deleteCarController,
-    listCarsController,
-    retriveCarsController,
-    updateCarController,
-    createCommentController,
-    listCommentsController,
-    listCommentsByIdController,
-    createImagesControllers,
-    loginController,
-    createUserController,
-    deleteUserController,
-    resetPasswordController,
-    retrieveUserController,
-    updateUserController
+    retrieveUserController, 
+    createUserController, 
+    updateUserController, 
+    sendEmailResetPasswordController, 
+    resetPasswordController, 
+    deleteUserController, 
+    loginController, 
+    createCarController, 
+    listCarsController, 
+    updateCarController, 
+    deleteCarController, 
+    retriveCarsController, 
+    listCommentsByIdController, 
+    createCommentController, 
+    listCommentsController, 
+    createImagesControllers 
 } from "../controllers";
-
 import { 
     ensureDataIsValidMiddleware, 
+    ensureEmailIsUniqueMiddleware, 
     ensureIdExistsMiddleware, 
     ensureUserIsAdvertiserMiddleware, 
     validateTokenMiddleware 
 } from "../middlewares";
-
 import { 
-    createCarSchema,
+    createCarSchema, 
     createCommentSchema, 
-    updateCarSchema } from "../schemas";
-    
-import { files } from "./images.routes";
+    createUserSchema, 
+    updateCarSchema, 
+    updateUserSchema 
+} from "../schemas";
+import { loginRoutes } from "./login.routes";
+import { carRoutes } from "./cars.routes";
+import { imagesRoutes } from "./images.routes";
+import multer from "multer";
+import { commentRoutes } from "./comments.routes";
 
 export const swaggerRoutes: Router = Router();
 
@@ -37,10 +44,27 @@ export const swaggerRoutes: Router = Router();
 
 /**
  * @swagger
- * tags:
- *   name: Users
- *   description: API endpoints for managing users
+ * /users/{userId}:
+ *   get:
+ *     summary: Retrieve a user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: ID of the user to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: User retrieved successfully
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
  */
+userRoutes.get("/:userId", retrieveUserController);
+
 
 /**
  * @swagger
@@ -64,30 +88,13 @@ export const swaggerRoutes: Router = Router();
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.post("/users", createUserController);
+userRoutes.post(
+    "", 
+    ensureDataIsValidMiddleware(createUserSchema), 
+    ensureEmailIsUniqueMiddleware, 
+    createUserController
+    );
 
-/**
- * @swagger
- * /users/{userId}:
- *   get:
- *     summary: Retrieve a user
- *     tags: [Users]
- *     parameters:
- *       - in: path
- *         name: userId
- *         required: true
- *         description: ID of the user to retrieve
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: User retrieved successfully
- *       404:
- *         description: User not found
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.get("/users/:userId", retrieveUserController);
 
 /**
  * @swagger
@@ -113,7 +120,13 @@ swaggerRoutes.get("/users/:userId", retrieveUserController);
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.patch("/users", validateTokenMiddleware, updateUserController);
+userRoutes.patch(
+  "",
+  validateTokenMiddleware,
+  ensureDataIsValidMiddleware(updateUserSchema),
+  updateUserController
+);
+
 
 /**
  * @swagger
@@ -141,7 +154,8 @@ swaggerRoutes.patch("/users", validateTokenMiddleware, updateUserController);
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.post("/users/resetPassword", resetPasswordController);
+userRoutes.post("/resetPassword", sendEmailResetPasswordController);
+
 
 /**
  * @swagger
@@ -176,270 +190,38 @@ swaggerRoutes.post("/users/resetPassword", resetPasswordController);
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.patch("/users/resetPassword/:token", resetPasswordController);
+userRoutes.patch("/resetPassword/:token", resetPasswordController);
 
-
-// -------------------------------- CARS --------------------------------
 
 /**
  * @swagger
- * tags:
- *   name: Cars
- *   description: API endpoints for managing cars
- */
-
-/**
- * 
- * @swagger
- * /cars:
- *   post:
- *     summary: Create a new car
- *     tags: [Cars]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/Car"
- *     responses:
- *       201:
- *         description: Car created successfully
- *       400:
- *         description: Invalid request payload
- *       401:
- *         description: Unauthorized access
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.post("/cars", validateTokenMiddleware, ensureUserIsAdvertiserMiddleware, ensureDataIsValidMiddleware(createCarSchema), createCarController);
-
-/**
- * @swagger
- * /cars:
- *   get:
- *     summary: List cars
- *     tags: [Cars]
- *     responses:
- *       200:
- *         description: Cars retrieved successfully
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.get("/cars", listCarsController);
-
-/**
- * @swagger
- * /cars/{id}:
- *   get:
- *     summary: Retrieve a car
- *     tags: [Cars]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of the car to retrieve
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Car retrieved successfully
-*        401:
-*         description: Unauthorized access
- *       404:
- *         description: Car not found
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.get("/cars/:id", validateTokenMiddleware, retriveCarsController);
-
-/**
- * @swagger
- * /cars/{id}:
- *   patch:
- *     summary: Update a car
- *     tags: [Cars]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID of the car to update
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/Car"
- *     responses:
- *       200:
- *         description: Car updated successfully
- *       400:
- *         description: Invalid request payload
- *       401:
- *         description: Unauthorized access
- *       404:
- *         description: Car not found
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.patch("/cars/:id", validateTokenMiddleware, ensureUserIsAdvertiserMiddleware, ensureIdExistsMiddleware, ensureDataIsValidMiddleware(updateCarSchema), updateCarController);
-
-/**
- * @swagger
- * /cars/{id}:
+ * /users/{id}:
  *   delete:
- *     summary: Delete a car
- *     tags: [Cars]
+ *     summary: Delete a user
+ *     tags: [Users]
  *     security:
  *       - bearerAuth: []
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
- *         description: ID of the car to delete
+ *         description: ID of the user to delete
  *         schema:
  *           type: string
  *     responses:
  *       200:
- *         description: Car deleted successfully
+ *         description: User deleted successfully
  *       401:
  *         description: Unauthorized access
  *       404:
- *         description: Car not found
+ *         description: User not found
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.delete("/cars/:id", validateTokenMiddleware, ensureUserIsAdvertiserMiddleware, ensureIdExistsMiddleware, deleteCarController);
+userRoutes.delete("/:id", validateTokenMiddleware, deleteUserController);
 
-// -------------------------------- COMMENTS --------------------------------
-
-/**
- * @swagger
- * tags:
- *   name: Comments
- *   description: API endpoints for managing comments
- */
-
-/**
- * @swagger
- * /comments/{userId}/{carId}:
- *   post:
- *     summary: Create a new comment
- *     tags: [Comments]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: userId
- *         required: true
- *         description: ID of the user creating the comment
- *         schema:
- *           type: string
- *       - in: path
- *         name: carId
- *         required: true
- *         description: ID of the car to comment on
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: "#/components/schemas/Comment"
- *     responses:
- *       201:
- *         description: Comment created successfully
- *       400:
- *         description: Invalid request payload
- *       401:
- *         description: Unauthorized access
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.post("/comments/:userId/:carId", validateTokenMiddleware, ensureDataIsValidMiddleware(createCommentSchema), createCommentController);
-
-/**
- * @swagger
- * /comments:
- *   get:
- *     summary: List comments
- *     tags: [Comments]
- *     responses:
- *       200:
- *         description: Comments retrieved successfully
-*        500:
-*         description: Internal server error
- */
-swaggerRoutes.get("/comments", listCommentsController);
-
-// -------------------------------- IMAGES --------------------------------
-
-/**
- * @swagger
- * tags:
- *   name: Images
- *   description: API endpoints for managing images
- */
-
-/**
- * @swagger
- * /images/{carId}:
- *   post:
- *     summary: Upload images for a car
- *     tags: [Images]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: carId
- *         required: true
- *         description: ID of the car to upload images for
- *         schema:
- *           type: string
- *     requestBody:
- *       content:
- *         multipart/form-data:
- *           schema:
- *             type: object
- *             properties:
- *               banner:
- *                 type: string
- *                 format: binary
- *                 description: Banner image file (maxCount: 1)
- *               photos:
- *                 type: string
- *                 format: binary
- *                 description: Array of photo image files (maxCount: 8)
- *     responses:
- *       200:
- *         description: Images uploaded successfully
- *       400:
- *         description: Invalid request payload
- *       401:
- *         description: Unauthorized access
- *       404:
- *         description: Car not found
- *       500:
- *         description: Internal server error
- */
-swaggerRoutes.post("/images/:carId", files, createImagesControllers);
 
 // -------------------------------- LOGIN --------------------------------
-
-/**
- * @swagger
- * tags:
- *   name: Authentication
- *   description: API endpoints for user authentication
- */
 
 /**
  * @swagger
@@ -470,6 +252,291 @@ swaggerRoutes.post("/images/:carId", files, createImagesControllers);
  *       500:
  *         description: Internal server error
  */
-swaggerRoutes.post("/login", loginController);
+loginRoutes.post("", loginController);
 
 
+// -------------------------------- CARS --------------------------------
+
+/**
+ * @swagger
+ * /cars:
+ *   post:
+ *     summary: Create a new car
+ *     tags: [Cars]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Car"
+ *     responses:
+ *       201:
+ *         description: Car created successfully
+ *       400:
+ *         description: Invalid request payload
+ *       401:
+ *         description: Unauthorized access
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.post(
+  "",
+  validateTokenMiddleware,
+  ensureUserIsAdvertiserMiddleware,
+  ensureDataIsValidMiddleware(createCarSchema),
+  createCarController
+);
+
+
+/**
+ * @swagger
+ * /cars:
+ *   get:
+ *     summary: List cars
+ *     tags: [Cars]
+ *     responses:
+ *       200:
+ *         description: Cars retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.get("", listCarsController);
+
+
+/**
+ * @swagger
+ * /cars/{id}:
+ *   patch:
+ *     summary: Update a car
+ *     tags: [Cars]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the car to update
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/UpdateCarInput"
+ *     responses:
+ *       200:
+ *         description: Car updated successfully
+ *       400:
+ *         description: Invalid request payload
+ *       401:
+ *         description: Unauthorized access
+ *       404:
+ *         description: Car not found
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.patch(
+  "/:id",
+  validateTokenMiddleware,
+  ensureUserIsAdvertiserMiddleware,
+  ensureIdExistsMiddleware,
+  ensureDataIsValidMiddleware(updateCarSchema),
+  updateCarController
+);
+
+
+/**
+ * @swagger
+ * /cars/{id}:
+ *   delete:
+ *     summary: Delete a car
+ *     tags: [Cars]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the car to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Car deleted successfully
+ *       401:
+ *         description: Unauthorized access
+ *       404:
+ *         description: Car not found
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.delete(
+  "/:id",
+  validateTokenMiddleware,
+  ensureUserIsAdvertiserMiddleware,
+  ensureIdExistsMiddleware,
+  deleteCarController
+);
+
+
+/**
+ * @swagger
+ * /cars/{id}:
+ *   get:
+ *     summary: Retrieve a car
+ *     tags: [Cars]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the car to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Car retrieved successfully
+ *       401:
+ *         description: Unauthorized access
+ *       404:
+ *         description: Car not found
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.get("/:id", validateTokenMiddleware, retriveCarsController);
+
+
+/**
+ * @swagger
+ * /cars/{carId}/comments:
+ *   get:
+ *     summary: Get comments by car ID
+ *     tags: [Cars]
+ *     parameters:
+ *       - in: path
+ *         name: carId
+ *         required: true
+ *         description: ID of the car to retrieve comments for
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Comments retrieved successfully
+ *       404:
+ *         description: Car not found
+ *       500:
+ *         description: Internal server error
+ */
+carRoutes.get("/:carId/comments", listCommentsByIdController);
+
+
+// -------------------------------- COMMENTS --------------------------------
+
+
+/**
+ * @swagger
+ * /comments/{carId}:
+ *   post:
+ *     summary: Create a new comment for a car
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: carId
+ *         required: true
+ *         description: ID of the car to add the comment to
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Comment"
+ *     responses:
+ *       201:
+ *         description: Comment created successfully
+ *       400:
+ *         description: Invalid request payload
+ *       401:
+ *         description: Unauthorized access
+ *       500:
+ *         description: Internal server error
+ */
+commentRoutes.post("/:carId", validateTokenMiddleware, ensureDataIsValidMiddleware(createCommentSchema), createCommentController);
+
+
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: Comments retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
+commentRoutes.get("", listCommentsController);
+
+
+// -------------------------------- IMAGES --------------------------------
+
+
+const storage = multer.diskStorage({
+  destination: "./tmp",
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+export const files = upload.fields([
+  { name: "banner", maxCount: 1 },
+  { name: "photos", maxCount: 8 },
+]);
+
+/**
+ * @swagger
+ * /images/{carId}:
+ *   post:
+ *     summary: Upload car images
+ *     tags: [Images]
+ *     parameters:
+ *       - in: path
+ *         name: carId
+ *         required: true
+ *         description: ID of the car to upload images for
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               banner:
+ *                 type: string
+ *                 format: binary
+ *               photos:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *             description: Array of photo image files
+ *           encoding:
+ *             photos:
+ *               maxItems: 8
+ *     responses:
+ *       200:
+ *         description: Images uploaded successfully
+ *       400:
+ *         description: Invalid request payload
+ *       500:
+ *         description: Internal server error
+ */
+imagesRoutes.post("/:carId", files, createImagesControllers);
